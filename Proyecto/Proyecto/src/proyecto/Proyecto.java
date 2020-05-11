8a4c1eb15e119b179a790ebb34e6ab4cb50bcc1f
Gonzalo <yayox588@gmail.com>
2020-05-10 22:34:30 -0400

    Hola gonzalo

--- Proyecto/Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/Proyecto/src/proyecto/Proyecto.java
@@ -10,12 +10,9 @@ package proyecto;
  * @author Juan
  */
 public class Proyecto {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Gonzalo");
     }
     
 }
