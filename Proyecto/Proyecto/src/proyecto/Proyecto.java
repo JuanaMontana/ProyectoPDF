5397d0aee714fc12b019ea008b16d726b927196b
Gonzalo <yayox588@gmail.com>
2020-05-10 22:35:53 -0400

    2da prueba

--- Proyecto/Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/Proyecto/src/proyecto/Proyecto.java
@@ -13,6 +13,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Gonzalo");
+        System.out.println("funciono");
     }
     
 }
