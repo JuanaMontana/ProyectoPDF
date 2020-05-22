a062a1b093f7dd8dd11fc479be480183e77bd5c6
juanjose <juanjo.farias.90@gmail.com>
2020-05-22 13:24:46 -0400

    fxml2
    asd

--- Proyecto/Proyecto/src/proyecto/MenuController.java
+++ Proyecto/Proyecto/src/proyecto/MenuController.java
@@ -5,6 +5,12 @@
  */
 package proyecto;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
