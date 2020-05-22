c4290755f9ce2a6013dd99b5379031deb83f17d9
juanjose <juanjo.farias.90@gmail.com>
2020-05-22 13:20:54 -0400

    fxml1
    das

--- Proyecto/Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/Proyecto/src/proyecto/Proyecto.java
@@ -15,7 +15,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         LecturaPdf lecturaPdf = new LecturaPdf();
-        lecturaPdf.setRutaDeArchivo("C:\\Users\\yayox\\Downloads\\Calculo 1 PDFS y guias\\G1U1.pdf");
+        lecturaPdf.setRutaDeArchivo("C:\\Users\\Juan\\Desktop\\2939-160-SE20.pdf");
         
         try{
             String texto = lecturaPdf.toText();
