6925514312f1785233ea69fe62916735fbea9e4c
juanjose <juanjo.farias.90@gmail.com>
2020-05-22 14:21:31 -0400

    fxlm3
    asddasdas

--- Proyecto/Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/Proyecto/src/proyecto/Proyecto.java
@@ -5,15 +5,36 @@
  */
 package proyecto;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
  *
  * @author Juan
  */
-public class Proyecto {
+public class Proyecto extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MenuInicial.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
         LecturaPdf lecturaPdf = new LecturaPdf();
         lecturaPdf.setRutaDeArchivo("C:\\Users\\Juan\\Desktop\\2939-160-SE20.pdf");
         
@@ -23,6 +44,10 @@ public class Proyecto {
         }catch (IOException ex){
             System.out.println(ex.getMessage());
         }
+        
+        launch(args);
+        
+        
         
     }
     
